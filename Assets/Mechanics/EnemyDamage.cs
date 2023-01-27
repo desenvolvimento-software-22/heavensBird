6e313c98d3668427a9843b22963b7b3dd448bde7
Diego-Carlito <diego.carlito01@gmail.com>
2023-01-26 21:00:45 -0300

    inclusao da barra de vida da Niru

---
+++ Assets/Mechanics/EnemyDamage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public playerhealth Playerhealth;
+    public int damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Player"){
+            Playerhealth.takeDamage(damage);
+        }
+    } 
+        
+    
+}
