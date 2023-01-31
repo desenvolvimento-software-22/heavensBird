a4ef6803f875ec284f23292d419c482e36a6b20d
Diego-Carlito <diego.carlito01@gmail.com>
2023-01-31 00:10:20 -0300

    desenvolvimento da cutscene e sistema de dialogo

---
+++ Assets/Player/Script/Dialogue.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public Sprite profile;
+    public string[] speechTxt;
+    public string actorName;
+
+    public LayerMask playerLayer;
+    public float radious;
+
+    private DialogueControl dc;
+    bool onRadious;
+
+    private void Start()
+    {
+        dc = FindObjectOfType<DialogueControl>();
+    }
+
+    private void FixedUpdate()
+    {
+        Interact();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && onRadious)
+        {
+            dc.Speech(profile, speechTxt, actorName);
+        }
+    }
+
+    public void Interact()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
+
+        if(hit != null)
+        {
+            onRadious = true;
+        }
+        else
+        {
+            onRadious = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radious);
+    }
+}
